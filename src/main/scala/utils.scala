615443eaf72d57e63c98b0f1cb3e1ecdf6fe338a
dustinvanstee <dustinvanstee@gmail.com>
2017-01-06 23:01:12 -0500

    Added support for object storage in bluemix

--- src/main/scala/utils.scala
+++ src/main/scala/utils.scala
@@ -1,5 +1,7 @@
 package dv.sparkbench.utils
 
+import org.apache.spark.SparkContext
+
 object funcs {
 
   // This function returns whatever the original function returns, plus the time in a Tuple
@@ -26,6 +28,24 @@ object funcs {
   	if(verbose) { 
   		println(s)
   	}
+  }
+
+
+  def connectToSwift(sc : SparkContext,  credentials : scala.collection.mutable.HashMap[String, String]) : Unit = {
+    val pfx = "fs.swift.service." + credentials("name") 
+    
+    val conf = sc.hadoopConfiguration
+    conf.set(pfx + ".auth.url", credentials("auth_url") + "/v3/auth/tokens" )
+    conf.set(pfx + ".auth.endpoint.prefix", "endpoints")
+    conf.set(pfx + ".tenant",   credentials("project_id"))
+    conf.set(pfx + ".username", credentials("user_id"))
+    conf.set(pfx + ".password", credentials("password"))
+    conf.setInt(pfx + ".http.port", 8080)
+    conf.set(pfx + ".region", credentials("region"))
+    conf.setBoolean(pfx + ".public", true)
+    
+    // conf.set(pfx + ".apikey",   credentials("password"))
+  
   }
 
 
